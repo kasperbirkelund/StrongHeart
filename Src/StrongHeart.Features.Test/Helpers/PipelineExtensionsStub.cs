767d167e4589a4b3f3b695c26ff12c4b1476a060
Kasper Birkelund Larsen <kasperbirkelund@gmail.com>
2021-06-18 09:42:29 +0200

    Add doc

--- Src/StrongHeart.Features.Test/Helpers/PipelineExtensionsStub.cs
+++ Src/StrongHeart.Features.Test/Helpers/PipelineExtensionsStub.cs
@@ -4,13 +4,14 @@ using StrongHeart.Features.Decorators.Authorization;
 using StrongHeart.Features.Decorators.ExceptionLogging;
 using StrongHeart.Features.Decorators.Filtering;
 using StrongHeart.Features.Decorators.RequestValidation;
+using StrongHeart.Features.Decorators.Retry;
 
 namespace StrongHeart.Features.Test.Helpers
 {
     public class PipelineExtensionsStub : List<IPipelineExtension>
     {
         //public FeatureAuditRepositorySpy AuditRepoSpy { get; } = new FeatureAuditRepositorySpy();
-        public ExceptionLoggerSpy ExceptionLoggerSpy { get; } = new ExceptionLoggerSpy();
+        public ExceptionLoggerSpy ExceptionLoggerSpy { get; } = new();
 
         public PipelineExtensionsStub()
         {
@@ -19,6 +20,7 @@ namespace StrongHeart.Features.Test.Helpers
             Add(new AuthorizationExtension());
             Add(new RequestValidatorExtension());
             Add(new FilterExtension());
+            Add(new RetryExtension());
         }
     }
 }
--- Src/StrongHeart.Features/DependencyInjection/FeatureSetupOptions.cs
+++ Src/StrongHeart.Features/DependencyInjection/FeatureSetupOptions.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 using StrongHeart.Features.Decorators;
+using StrongHeart.Features.Decorators.Authorization;
+using StrongHeart.Features.Decorators.ExceptionLogging;
+using StrongHeart.Features.Decorators.Filtering;
+using StrongHeart.Features.Decorators.RequestValidation;
+using StrongHeart.Features.Decorators.Retry;
 
 namespace StrongHeart.Features.DependencyInjection
 {
@@ -28,6 +34,25 @@ namespace StrongHeart.Features.DependencyInjection
                 AddPipelineExtension(item);
             }
             return this;
+        }
+
+        public FeatureSetupOptions AddDefaultPipeline(Func<IExceptionLogger> loggerFunc)
+        {
+            AddPipelineExtensions(new DefaultPipelineExtensions(loggerFunc));
+            return this;
+        }
+
+        private class DefaultPipelineExtensions : List<IPipelineExtension>
+        {
+            public DefaultPipelineExtensions(Func<IExceptionLogger> loggerFunc)
+            {
+                //First = outermost
+                Add(new ExceptionLoggerExtension(loggerFunc)); //1: we want to have exception handling outermost to ensure that everything gets logged
+                Add(new AuthorizationExtension()); //2: The user must be authorized before we expose any further 
+                Add(new RequestValidatorExtension()); //3: Now it is time for input validation
+                Add(new FilterExtension()); //4: Filtering only applies to queries
+                Add(new RetryExtension()); //5: it makes sense to have retry closest to the real feature
+            }
         }
     }
 }
