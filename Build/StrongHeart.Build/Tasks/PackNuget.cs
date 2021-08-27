21a2b0fb08620647f635e27f35cef6cf965e4c5c
Kasper Birkelund Larsen <kasperbirkelund@gmail.com>
2021-08-27 13:10:15 +0200

    Add nuget feed

---
+++ Build/StrongHeart.Build/Tasks/PackNuget.cs
@@ -0,0 +1,34 @@
+﻿using Cake.Common.IO;
+using Cake.Common.Tools.DotNetCore;
+using Cake.Frosting;
+using StrongHeart.Build.Tasks.Utilities;
+
+namespace StrongHeart.Build.Tasks
+{
+    [Dependency(typeof(CiBuild))]
+    public class PackNuget : FrostingTask<StrongHeartBuildContext>
+    {
+        public override void Run(StrongHeartBuildContext context)
+        {
+            context.DeleteFiles("**/*.nupkg");
+            context.DeleteFiles("**/*.nuspec");
+
+            string[] projectsToNugetPack =
+            {
+                "StrongHeart.Features",
+                "StrongHeart.Features.AspNetCore",
+                "StrongHeart.Features.Documentation",
+                "StrongHeart.Core",
+                "StrongHeart.EfCore",
+                "StrongHeart.Migrations",
+                "StrongHeart.TestTools.ComponentAnalysis",
+                "StrongHeart.TestTools.Xunit"
+            };
+
+            foreach (string project in projectsToNugetPack)
+            {
+                context.DotNetCorePack(@$".\Src\{project}\{project}.csproj", context.GetDotNetCorePackSettings());
+            }
+        }
+    }
+}
--- Build/StrongHeart.Build/Tasks/Utilities/ContextExtensions.cs
+++ Build/StrongHeart.Build/Tasks/Utilities/ContextExtensions.cs
@@ -1,15 +1,62 @@
 using System;
+using Cake.Common.Build;
 using Cake.Common.Diagnostics;
 using Cake.Common.Tools.DotNetCore;
 using Cake.Common.Tools.DotNetCore.Build;
 using Cake.Common.Tools.DotNetCore.MSBuild;
+using Cake.Common.Tools.DotNetCore.NuGet.Push;
+using Cake.Common.Tools.DotNetCore.Pack;
 using Cake.Common.Tools.DotNetCore.Test;
+using Cake.Core;
+using Cake.Frosting;
 
 namespace StrongHeart.Build.Tasks.Utilities
 {
+    public record NugetSourceInfo(string? ApiKey, string Source);
     public static class ContextExtensions
     {
-        private const DotNetCoreVerbosity Verbosity = DotNetCoreVerbosity.Minimal;
+        private const DotNetCoreVerbosity Verbosity = DotNetCoreVerbosity.Quiet;
+
+        public static DotNetCoreNuGetPushSettings GetDotNetCoreNuGetPushSettings(this FrostingContext context)
+        {
+            NugetSourceInfo sourceInfo = GetNugetSourceInfo(context.GitHubActions().IsRunningOnGitHubActions);
+            
+            return new DotNetCoreNuGetPushSettings
+            {
+                Source = sourceInfo.Source,
+                ApiKey = sourceInfo.ApiKey,
+                SkipDuplicate = false,
+                ForceEnglishOutput = true
+            };
+        }
+
+        private static NugetSourceInfo GetNugetSourceInfo(bool isBuildServer)
+        {
+            //dotnet nuget push "**/*.nupkg" - s C:\development\NugetFeed
+            if (!isBuildServer)
+            {
+                return new NugetSourceInfo(null, @"C:\development\NugetFeed");
+            }
+            throw new NotImplementedException("");
+            //return new NugetSourceInfo(null, "https://api.nuget.org/v3/index.json");
+        }
+
+        public static DotNetCorePackSettings GetDotNetCorePackSettings(this StrongHeartBuildContext context)
+        {
+            BuildVersion version = BuildVersion.Calculate(context);
+            return new()
+            {
+                Configuration = context.BuildConfiguration,
+                NoLogo = true,
+                NoBuild = true,
+                Verbosity = Verbosity,
+                ArgumentCustomization = builder =>
+                {
+                    builder.Append($"/p:Version={version.AssemblySemVer}");
+                    return builder;
+                }
+            };
+        }
 
         public static DotNetCoreTestSettings GetDotNetCoreTestSettings(this StrongHeartBuildContext context)
         {
@@ -38,7 +85,7 @@ namespace StrongHeart.Build.Tasks.Utilities
             return settings;
         }
 
-        private static DotNetCoreMSBuildSettings GetDotNetCoreMsBuildSettings(StrongHeartBuildContext context)
+        private static DotNetCoreMSBuildSettings GetDotNetCoreMsBuildSettings(FrostingContext context)
         {
             BuildVersion version = BuildVersion.Calculate(context);
             context.Information(version.ToString());
