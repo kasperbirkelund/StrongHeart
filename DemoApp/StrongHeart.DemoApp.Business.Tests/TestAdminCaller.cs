f2aaf3c86670aebed4ab43918ab3abd8cc3f84bf
Kasper Birkelund Larsen <kasperbirkelund@gmail.com>
2022-03-06 22:13:41 +0100

    Add draft TestExecutor

---
+++ DemoApp/StrongHeart.DemoApp.Business.Tests/TestAdminCaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Claims;
+using StrongHeart.Core.Security;
+using StrongHeart.Features.Core;
+
+namespace StrongHeart.DemoApp.Business.Tests;
+
+[DebuggerStepThrough]
+public class TestAdminCaller : ICaller
+{
+    public Guid Id { get; } = new Guid("dd2219e8-2e2a-44ca-9722-a93472c124b9");
+
+    public IReadOnlyList<Claim> Claims { get; } = new List<Claim>()
+    {
+        AdminClaim.Instance
+    }.AsReadOnly();
+}
---
+++ DemoApp/StrongHeart.DemoApp.Business.Tests/TestExecutor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using StrongHeart.Features.Core;
+
+namespace StrongHeart.DemoApp.Business.Tests
+{
+    public class TestExecutor : IDisposable
+    {
+        private readonly bool _checkForSuccessExecutionResult;
+
+        private TestExecutor(IServiceCollection services, bool checkForSuccessExecutionResult)
+        {
+            _checkForSuccessExecutionResult = checkForSuccessExecutionResult;
+            var provider = services.BuildServiceProvider();
+            Scope = provider.CreateScope();
+        }
+
+        public static TestExecutor Create(IServiceCollection services)
+        {
+            return new TestExecutor(services, true);
+        }
+
+        public IServiceScope Scope { get; }
+
+        public void Dispose()
+        {
+            Scope.Dispose();
+        }
+
+        public async Task<Result<TResponse>> RunQuery<TRequest, TResponse>(TRequest request)
+            where TRequest : IRequest
+            where TResponse : class, IResponseDto
+        {
+            var feature = Scope.ServiceProvider.GetRequiredService<IQueryFeature<TRequest, TResponse>>();
+            Result<TResponse> result = await feature.Execute(request);
+            CheckStatus<TRequest>(result);
+
+            return result;
+        }
+
+        public async Task<Result> RunCommand<TRequest, TRequestDto>(TRequest request) 
+            where TRequestDto : IRequestDto 
+            where TRequest : IRequest<TRequestDto>
+        {
+            var feature = Scope.ServiceProvider.GetRequiredService<ICommandFeature<TRequest, TRequestDto>>();
+            Result result = await feature.Execute(request);
+            CheckStatus<TRequest>(result);
+            
+            return result;
+        }
+
+        private void CheckStatus<TRequest>(IResult result)
+        {
+            if (_checkForSuccessExecutionResult && !result.IsSuccess)
+            {
+                throw new InvalidOperationException($"Execution failed on request {typeof(TRequest).Name}: {result.Error}");
+            }
+        }
+    }
+}
