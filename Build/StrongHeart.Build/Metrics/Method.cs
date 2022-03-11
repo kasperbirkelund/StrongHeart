3827e2c7d90435b6abc5a132f6ce31f8fb2532e7
Kasper Birkelund Larsen <kasperbirkelund@gmail.com>
2022-03-11 23:29:07 +0100

    Add metrics calculator

---
+++ Build/StrongHeart.Build/Metrics/Method.cs
@@ -0,0 +1,9 @@
+﻿using System.Xml.Serialization;
+
+namespace StrongHeart.Build.Metrics;
+
+public class Method
+{
+    [XmlAttribute("Name")]public string? Name { get; set; }
+    public Metric[]? Metrics { get; set; }
+}
