3d2fcb54ffbe18a1fb28fc20bb3894d150d651e6
Kasper Birkelund Larsen <kasperbirkelund@gmail.com>
2022-03-12 16:52:43 +0100

    Refactor

--- Build/StrongHeart.Build/EntryPoints/Default.cs
+++ Build/StrongHeart.Build/EntryPoints/Default.cs
@@ -4,7 +4,7 @@ using StrongHeart.Build.Tasks;
 namespace StrongHeart.Build.EntryPoints
 {
     //[Dependency(typeof(PushNuget))]
-    //[Dependency(typeof(CiBuild))]
+    [Dependency(typeof(CiBuild))]
     [Dependency(typeof(CalculateMetrics))]
     public class Default : FrostingTask
     {
